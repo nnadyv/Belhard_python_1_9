00ba66db52e029fa8a05cd4ae703b8c4078379ef
nadya <naah@gmail.com>
2022-09-04 03:50:46 +0200

    did datetime_tasks

--- tasks/datetime_tasks/time_converter.py
+++ tasks/datetime_tasks/time_converter.py
@@ -6,3 +6,16 @@
 
 Функция должна возвращать время в новой временной зоне.
 """
+import datetime
+
+import pytz
+
+
+def convert_date(timestamp, current_zone, new_zone):
+    current_zone = pytz.timezone(current_zone)
+    new_zone = pytz.timezone(new_zone)
+    timestamp = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+    timestamp = current_zone.localize(timestamp)
+    timestamp = timestamp.astimezone(new_zone)
+    timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+    return timestamp
